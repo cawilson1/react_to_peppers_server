157f08e306e972208f083655e60fc7ba76f5f776
cawilson1 <cawilson1@g.cofc.edu>
2020-09-24 09:38:56 -0400

    create pepper table

--- dbseed.js
+++ dbseed.js
@@ -17,14 +17,42 @@ const pool = sql.createPool({
 //   }
 // })();
 
-(async function createUserTable() {
+// (async function createUserTable() {
+//   try {
+//     const conn = await pool.getConnection();
+
+//     conn.query("CREATE DATABASE IF NOT EXISTS peppers");
+//     conn.query("USE peppers");
+//     const userDb = await conn.query(
+//       "CREATE TABLE IF NOT EXISTS user (username VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(username))"
+//     );
+//     console.log(userDb);
+//     conn.release();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
+
+// name: "Golden Cayenne", //VARCHAR(255) NOT NULL
+// scoville: 40000, //heat //INT NOT NULL
+// pic:
+//   "https://edge.bonnieplants.com/www/tiny/uploads/20200810205412/golden-cayenne-pepper.jpg", //VARCHAR 2000 NOT NULL
+// flavor: "spicy, sweet, apple-y", //VARCHAR (2000)
+// color: "yellow", //VARCHAR (255)
+// species: "capsicum annuum", //VARCHAR 255
+// growthTimeMonths: 1, //TINYINT
+// size: 4, //1-10 (1 smallest) //TINYINT
+// id: 1, //INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY
+// user: "Casey" //VARCHAR 255 FOREIGN KEY
+
+(async function createPepperTable() {
   try {
     const conn = await pool.getConnection();
 
     conn.query("CREATE DATABASE IF NOT EXISTS peppers");
     conn.query("USE peppers");
     const userDb = await conn.query(
-      "CREATE TABLE IF NOT EXISTS user (username VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(username))"
+      "CREATE TABLE IF NOT EXISTS pepper (id INT AUTO_INCREMENT NOT NULL UNIQUE, name VARCHAR(255) NOT NULL, scoville INT NOT NULL, pic VARCHAR(2000) NOT NULL, flavor VARCHAR(2000), color VARCHAR(255), species VARCHAR(255), growthtimemonths TINYINT, size TINYINT, user VARCHAR(255),  PRIMARY KEY(id), FOREIGN KEY (user) REFERENCES user(username))"
     );
     console.log(userDb);
     conn.release();
